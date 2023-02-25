f8e4312d06d12d12b19e92971e5b3362559dc8ad
Michael Ngo <mikono@dhcp-vl2041-39820.eduroam.cornell.edu>
2023-02-25 06:26:01 -0500

    fix more pylint app errors

--- view/app.py
+++ view/app.py
@@ -1,10 +1,11 @@
 """
 Frontend
 """
+import time
 import streamlit as st
 import hydralit_components as hc
 import pandas as pd
-import time
+
 
 
 
@@ -15,14 +16,12 @@ import time
 def update_video(video_file):
     st.session_state.video_file = video_file
 
-# Send request to Google Compute Machine 
+# Send request to Google Compute Machine
 def process_video(video_file):
     time.sleep(5)
+    return video_file is not None
 
 # ------------------------------------------------------------
-
-
-
 
 
 # Set up tab title and favicon
@@ -49,12 +48,12 @@ def main_page():
         A basketball analytics tracker built on YOLOv5 and OpenCV.
         Simply upload a video in the side bar and click "Process Video."
     ''')
-    
+
     # send to tips page
-    tips_button = st.button(label="Having Trouble?", on_click=change_state, args=(-1,))
+    st.button(label="Having Trouble?", on_click=change_state, args=(-1,))
 
     # Basketball Icon Filler
-    col1, col2, col3 = st.columns([0.5,5,0.5])
+    _, col2, _ = st.columns([0.5,5,0.5])
     with col2:
         st.image(image=st.session_state.logo,use_column_width=True)
 
@@ -68,20 +67,17 @@ def tips_page():
         * Ensure the players and hoop and the court are visible as much as possible.
         * We recommend at least 1080p video shot at 30 fps.
     ''')
-    
+
     # Back to Home
-    back_button = st.button(label='Back to Home', on_click=change_state, args=(0,))
+    st.button(label='Back to Home', on_click=change_state, args=(0,))
 
 # Loading Screen
 def processing_page():
     st.markdown('''
         # Processing...
-        Please wait while we are processing your video. 
-        Your call is very important to us. 
-        Thank you for shopping at Aperture Science.
-
+        Please wait while we upload and process your video. 
     ''')
-    
+
     # Loading bar until video processes
     with hc.HyLoader('', hc.Loaders.pulse_bars,):
         process_video(video_file=st.session_state.video_file)
@@ -122,9 +118,12 @@ def results_page():
 
     st.markdown('### Raw Data')
     st.write(st.session_state.results)
-    st.download_button(label='Download CSV', data=st.session_state.results.to_csv(), file_name="results.csv")
-    
+    st.download_button(
+        label='Download CSV',
+        data=st.session_state.results.to_csv(),
+        file_name="results.csv")
 
+# Error Page
 def error_page():
     st.markdown('''
         # Error: Webpage Not Found
@@ -132,14 +131,15 @@ def error_page():
         ''')
     st.button(label='Back to Home', on_click=change_state, args=(0,))
 
-# Sidebar
 def setup_sidebar():
-
     # Display upload file widget
     st.sidebar.markdown('# Upload')
-    file_uploader = st.sidebar.file_uploader(label='Upload a video', type=['mp4', 'mov', 'wmv', 'avi', 'flv', 'mkv'], label_visibility='collapsed')
+    file_uploader = st.sidebar.file_uploader(
+        label='Upload a video',
+        type=['mp4', 'mov', 'wmv', 'avi', 'flv', 'mkv'],
+        label_visibility='collapsed')
     if file_uploader is not None:
-            update_video(file_uploader)
+        update_video(file_uploader)
 
     # Display video they uploaded
     st.sidebar.markdown('# Your video')
@@ -147,28 +147,30 @@ def setup_sidebar():
 
     # Process options to move to next state
     col1, col2 = st.sidebar.columns([1,17])
-    with st.sidebar.container():
-        consent_check = col1.checkbox(label=" ", label_visibility='hidden')
-        col2.caption("I have read and agree to HoopTrack's [terms of services.](https://github.com/CornellDataScience/Ball-101)")
-    
+    consent_check = col1.checkbox(label=" ", label_visibility='hidden')
+    col2.caption('''
+        I have read and agree to HoopTrack's
+        [terms of services.](https://github.com/CornellDataScience/Ball-101)
+    ''')
+
     st.sidebar.button(label='Upload & Process Video',
                       disabled= not consent_check,
-                      use_container_width=True, 
-                      on_click=change_state, args=(1,), 
+                      use_container_width=True,
+                      on_click=change_state, args=(1,),
                       type='primary')
 
     # Write Export Option when in results page
     if st.session_state.state == 2:
         st.sidebar.markdown('# Export')
-        st.sidebar.download_button(label='Download CSV', use_container_width=True, data=st.session_state.results.to_csv(), file_name="results.csv")
- 
+        st.sidebar.download_button(
+            label='Download CSV',
+            use_container_width=True,
+            data=st.session_state.results.to_csv(),
+            file_name="results.csv")
 
-
-
-
+# Call back function to change page
 def change_state(state:int):
     st.session_state.state = state
-
 
 # Set home info page test
 if st.session_state.state == -1:
@@ -183,5 +185,3 @@ else:
     error_page()
 
 setup_sidebar()
-
-
