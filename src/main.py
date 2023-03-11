f38c35b1cab2e629c35ec3864e600280a3054144
Benson Zhang <bjz22@cornell.edu>
2023-03-11 17:56:31 -0500

    infra restructuring

--- src/main.py
+++ src/main.py
@@ -3,10 +3,17 @@ Control Loop Module
 """
 import os
 import sys
+import yaml
+import argparse
 from api import gcs
 from . import modelrunner
 # import statrunner
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+def load_config(path):
+	with open(path, 'r') as f:
+		config = yaml.safe_load(f)
+	return config
 
 
 def pull_user_video(file_name):
@@ -18,3 +25,15 @@ def pull_user_video(file_name):
 	user_file_path = 'model/temp/user.mp4'
 	gcs.download_from_bucket(file_name, user_file_path)
 	return modelrunner.run_models(user_file_path)
+
+
+def main():
+	config = load_config('config.yaml')
+	parser = argparse.ArgumentParser(prog='HoopTracker', 
+				  description='feed file into the model')
+	parser.add_argument('--ci', metavar='file_name', type=str, required=True,
+		     help='path of the file to run processing through')
+	
+
+if __name__ == "__main__":
+	main()
