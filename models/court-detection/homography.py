42d45e85113292686eb082ed4fbe14c3536aea02
Michael Ngo <mikono@dhcp-vl2041-39820.eduroam.cornell.edu>
2023-03-11 12:40:16 -0500

    ycrcb binning and contours

--- models/court-detection/homography.py
+++ models/court-detection/homography.py
@@ -8,82 +8,105 @@ COURT_PATH = os.path.join(TEMP_PATH,'court.jpeg')
 TEMPLATE_PATH = os.path.join(TEMP_PATH,'template.jpeg')
 
 def main():
-    img = cv.imread(COURT_PATH)
-    bins = bin_pixels(img, cr_bins=16, cb_bins=16)
-    masked_img = apply_mask(img, bins)
     # test_homography(COURT_PATH)
-    # harris_corners(COURT_PATH)
-    # tomasi_corners(COURT_PATH)
-    # sift_corners(COURT_PATH)
+    bgr_image = cv.imread(COURT_PATH)
+    ycrcb_img = cv.cvtColor(bgr_image,cv.COLOR_BGR2YCrCb)
 
-    # harris_corners(TEMPLATE_PATH)
-    # sift_corners(TEMPLATE_PATH)
+    bins = bin_pixels(ycrcb_img, cr_bins=10, cb_bins=10)
+
+    mask = get_mask(ycrcb_img, bins[1])
+    res = apply_mask(bgr_image,mask)
+
+    contours = get_hull(mask)
+    res2 = apply_hull(bgr_image,contours)
 
     # convert_to_hsv(COURT_PATH)
     # convert_to_ycrcb(COURT_PATH)
 
-    cv.imshow("original", img)
-    cv.imshow("masked image", masked_img)
+    cv.imshow("original",bgr_image)
+    cv.imshow("mask",mask)
+    cv.imshow("masked image",res)
+    cv.imshow("image with contours",res2)
 
     if cv.waitKey(0) & 0xff == 27:
-        print('got here')
         cv.destroyAllWindows()
 
+class YCrBrBin:
+    def __init__(self, value, cr_lower:int, cr_upper:int, cb_lower:int, cb_upper:int):
+        self.cr_lower = cr_lower
+        self.cr_upper = cr_upper
+        self.cb_lower = cb_lower
+        self.cb_upper = cb_upper
+        self.value = value
+
+    def __str__(self):
+        return 'YCrCbBin('+str(round(self.value,3))+','+str(self.cr_lower)+','+str(self.cr_upper)+','+str(self.cb_lower)+','+str(self.cb_upper)+')'
+
 # Returns top bins from YCrCb color space
+# @Precondition: img is YCrCb converted image
+# @Returns: sorted array of most frequent YCrCb bin objects
 def bin_pixels(img:np.ndarray, cr_bins:int=16, cb_bins:int=16):
-    # prepare bin
-    bins = [[[0,0,0] for j in range(cb_bins)] for i in range(cr_bins)]
-    cr_step = 255.0 / cr_bins
-    cb_step = 255.0 / cb_bins
-    cr_lower, cr_upper = 0, cr_step
-    for row in range(cr_bins):
-        cb_lower, cb_upper = 0, cb_step
-        for col in range(cb_bins):
-            bins[row][col][1] = (int(round(cr_lower)),int(round(cr_upper)))
-            bins[row][col][2] = (int(round(cb_lower)),int(round(cb_upper)))
-            cb_lower += cb_step
-            cb_upper += cb_step
-        cb_lower
-        cr_lower += cr_step
-        cr_upper += cr_step
+    # generate weights
+    weights = np.zeros((img.shape[0],img.shape[1]))
+    for row in range(weights.shape[0]):
+        row_weight = np.full(weights.shape[1],min(row+1,weights.shape[0]-row))
+        weights[row] = row_weight
+    for col in range(weights.shape[1]):
+        col_weight = np.full(weights.shape[0],min(col+1,weights.shape[1]-col))
+        weights[:,col] = np.minimum(weights[:,col],col_weight)
+    weights = (weights*(weights.shape[0]/2.0))**2
+    weights = weights/np.sum(weights) 
 
     # split image pixels into bins
-    src = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
-    for line in src:
-        for pix in line: # TODO: implement scaling around part of video
-            bins[int(pix[1]/cr_step)][int(pix[2]/cb_step)][0] += 1
-
-    # normalize counts
-    total = 0
-    for row in bins:
-        for bin in row:
-            total += bin[0]
-    for row in bins:
-        for bin in row:
-            bin[0] = round(bin[0] / float(total), ndigits=4)
+    bins = np.zeros((cr_bins,cb_bins))
+    cr_step = 255.0 / cr_bins
+    cb_step = 255.0 / cb_bins
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            pix = img[row,col]
+            bins[int(pix[1]/cr_step),int(pix[2]/cb_step)] += weights[row,col]
 
     # sort bins
-    top_bins = [bin for row in bins for bin in row if bin[0]>0.001]
-    sorted_bins = sorted(top_bins, reverse=True)
-    return sorted_bins
+    top_bins = []
+    THRESHOLD = 0.001
+    for row in range(bins.shape[0]):
+        for col in range(bins.shape[1]):
+            if (bins[row,col] <= THRESHOLD):
+                continue
+            top_bins.append(YCrBrBin(bins[row,col],
+                            int(round(cr_step*row)),
+                            int(round(cr_step*(row+1))),
+                            int(round(cb_step*col)),
+                            int(round(cb_step*(col+1)))))
+    return sorted(top_bins, reverse=True, key=lambda bin: bin.value)
     
 # applies color range from top bin to image
-def apply_mask(img:np.ndarray, bins:list):
+def get_mask(img:np.ndarray, bin:YCrBrBin):
     # get mask
-    lower_cr, upper_cr = bins[0][1]
-    lower_cb, upper_cb = bins[0][2]
-    lowerbound = np.array([0,lower_cr,lower_cb])
-    upperbound = np.array([255,upper_cr,upper_cb])
-    src = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
-    mask = cv.inRange(src, lowerbound, upperbound)
-    cv.imshow("masked image", mask)
+    lowerbound = np.array([0,bin.cr_lower,bin.cb_lower])
+    upperbound = np.array([255,bin.cr_upper,bin.cb_upper])
+    mask = cv.inRange(img, lowerbound, upperbound)
+    cv.imshow('original mask',mask)
 
-    # apply mask
-    res = cv.bitwise_and(src,src,mask=mask)
-    return cv.cvtColor(res,cv.COLOR_YCrCb2BGR)
+    # close and open mask
+    kernel = np.ones((3,3),np.uint8)
+    mask = cv.morphologyEx(mask,cv.MORPH_CLOSE,kernel,iterations=4)
+    mask = cv.morphologyEx(mask,cv.MORPH_OPEN,kernel,iterations=3)
+    return mask 
 
+# @Precondition: img should be BGR color space
+def apply_mask(img:np.ndarray, mask:np.ndarray):
+    return cv.bitwise_and(img,img,mask=mask)
 
+def get_hull(mask:np.ndarray):
+    ret, thresh = cv.threshold(mask,127,255,0)
+    contours, hierachy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    return contours
 
+def apply_hull(img:np.ndarray, contours:tuple):
+    copy = img.copy()
+    cv.drawContours(copy, contours, -1, (0,255,0), 1)
+    return copy
 
 def test_homography(source_image:str):
     im_src = cv.imread(source_image)
@@ -92,52 +115,12 @@ def test_homography(source_image:str):
     im_dst = cv.imread(TEMPLATE_PATH)
     pts_dst = np.array([[38,13],[571,13],[571,339],[38,339]])
 
-    h, status = cv.findHomography(pts_src,pts_dst)
+    h, _ = cv.findHomography(pts_src,pts_dst)
     im_out = cv.warpPerspective(im_src, h, (im_dst.shape[1],im_dst.shape[0]))
     
     cv.imshow("Source Image", im_src)
     cv.imshow("Destination Image", im_dst)
     cv.imshow("Warped Source Image", im_out)  
-
-def harris_corners(source_image:str):
-    img = cv.imread(source_image)
-
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    gray_arr = np.float32(gray)
-    dst = cv.cornerHarris(gray_arr,20,3,0.04)
-
-    # dilate to original size
-    dst = cv.dilate(dst,None)
-    harris = img.copy()
-    harris[dst>0.05*dst.max()]=[0,0,255]
-
-    cv.imshow('Harris Corner Detector',harris)
-
-def tomasi_corners(source_image:str):
-    img = cv.imread(source_image)
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-
-    corners = cv.goodFeaturesToTrack(gray,25,0.01,10)
-    corners = np.int0(corners)
-
-    #draw circles onto image
-    tomasi = img.copy()
-    for i in corners:
-        x,y = i.ravel()
-        cv.circle(tomasi,(x,y),3,255,-1)
-
-    cv.imshow('Shi-Tomasi Corner Detector',tomasi)
-
-def sift_corners(source_image:str):
-    img = cv.imread(source_image)
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-
-    sift = cv.SIFT_create()
-    kp = sift.detect(gray,None)
-
-    sift_img = cv.drawKeypoints(gray,kp,img)
-
-    cv.imshow('SIFT Corner Detection',sift_img)
 
 def convert_to_hsv(source_image:str):
     img = cv.imread(source_image)
@@ -171,16 +154,16 @@ def convert_to_ycrcb(source_image:str):
     ycrcb = cv.cvtColor(img,cv.COLOR_BGR2YCrCb)
 
     luma_only = ycrcb.copy()
-    luma_only[:,:,1] = 125
-    luma_only[:,:,2] = 125
+    luma_only[:,:,1] = 128
+    luma_only[:,:,2] = 128
 
     blue_only = ycrcb.copy()
-    blue_only[:,:,0] = 125
-    blue_only[:,:,2] = 125
+    blue_only[:,:,0] = 0
+    blue_only[:,:,2] = 128
 
     red_only = ycrcb.copy()
-    red_only[:,:,0] = 125
-    red_only[:,:,1] = 125
+    red_only[:,:,0] = 0
+    red_only[:,:,1] = 128
 
     luma_only = cv.cvtColor(luma_only, cv.COLOR_YCrCb2BGR)
     blue_only = cv.cvtColor(blue_only, cv.COLOR_YCrCb2BGR)
