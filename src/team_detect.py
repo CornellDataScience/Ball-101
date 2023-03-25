02da1147506bdd9640ab04eaa0dc9fe380d62b6f
Pun Chaixanien <sc2343@cornell.edu>
2023-03-25 14:32:58 -0400

    cleaned team detect

--- src/team_detect.py
+++ src/team_detect.py
@@ -1,52 +1,65 @@
+"""Team Detection and Possession Finder
+
+This module contains functions that will find the best team split. It will
+also create a list of players in the order of ball possession.
+"""
+
 def curr_possession(players, ball):
     '''
     Input:
         players: dictionary of players
         ball: dictionary containing coords of the ball
     Output:
-        possession_list [list]: list of player ids in the order of ball possession throughout the video
+        possession_list [list]: list of player ids in the order of ball
+                                possession throughout the video
     '''
     player_ids = players.keys()
-    maxArea = 0
-    maxPlayer = None
+    max_area = 0
+    max_player = None
     bxmin = ball.get("xmin")
     bxmax = ball.get("xmax")
     bymin = ball.get("ymin")
     bymax = ball.get("ymax")
     for player in player_ids:
         pcoords = players.get(player)
-        currxmin = max(pcoords.get("xmin"), bxmin)
-        currxmax = min(pcoords.get("xmax"), bxmax)
-        currymin = max(pcoords.get("ymin"), bymin)
-        currymax = min(pcoords.get("ymax"), bymax)
-        area = (currxmax - currxmin) * (currymax - currymin)
-        if area > maxArea:
-            maxArea = area
-            maxPlayer = player
-    return maxPlayer
+        curr_xmin = max(pcoords.get("xmin"), bxmin)
+        curr_xmax = min(pcoords.get("xmax"), bxmax)
+        curr_ymin = max(pcoords.get("ymin"), bymin)
+        curr_ymax = min(pcoords.get("ymax"), bymax)
+        area = (curr_xmax - curr_xmin) * (curr_ymax - curr_ymin)
+        if area > max_area:
+            max_area = area
+            max_player = player
+    return max_player
 
 def possession_list(state, thresh=20):
     '''
     Input:
-        state: a StatState class that holds all sorts of information on the video
-        thresh: number of frames for ball overlapping with player in order to count as possession
+        state: a StatState class that holds all sorts of information
+                on the video
+        thresh: number of frames for ball overlapping with player
+                in order to count as possession
     Output:
-        possession_list [list]: list of player ids in the order of ball possession throughout the video
+        possession_list [list]: list of player ids in the order of ball
+        possession throughout the video
     '''
-    # list of frames where a frame is a dictionary with key classes and values xmin, ymin, xmax, ymax
-    # for key = players value has the format {players: {playerid1: {xmin: val, ymin: val, xmax: val, ymax: val}}}
+    # list of frames where a frame is a dictionary with key classes and
+    # values xmin, ymin, xmax, ymax
+    # for key = players value has the format {players:
+    # {playerid1: {xmin: val, ymin: val, xmax: val, ymax: val}}}
     states = state.states
     counter = 0
     current_player = None
     pos_lst = []
     for frame in states:
-        if frame.get("ball") == None:
+        if frame.get("ball") is None:
             continue
         poss = curr_possession(frame.get("players"), frame.get("ball"))
 
-        if poss == None:
+        if poss is None:
             continue
-        elif poss == current_player:
+
+        if poss == current_player:
             counter += 1
         else:
             current_player = poss
@@ -57,13 +70,22 @@ def possession_list(state, thresh=20):
     return pos_lst
 
 def connections(pos_lst, players):
-    connects = dict()
-    for i in range(0, len(players)):
-        for j in range(0, len(players)):
+    """
+    Input:
+        pos_lst [list]: list of player ids in the order of ball possession
+                        throughout the video
+        players [list]: list of player ids
+    Output:
+        connects [dict]: dictionary of connections between players
+    """
+    connects = {}
+    for i, player in enumerate(players):
+        for j, player2 in enumerate(players):
             if i == j:
                 continue
-            name = players[i] + players[j]
+            name = player + player2
             connects.update({name: 0})
+
     curr = pos_lst[0]
     for i in range(1, len(pos_lst)):
         name = curr + pos_lst[i]
@@ -71,13 +93,19 @@ def connections(pos_lst, players):
     return connects
 
 def possible_teams(players):
-    numpeople = len(players)
-    pplperteam = numpeople/2
+    """
+    Input:
+        players [list]: list of player ids
+    Output:
+        acc [list]: list of possible team splits
+    """
+    num_people = len(players)
+    ppl_per_team = num_people/2
     acc = []
     def permutation(i, t):
-        if i >= numpeople:
+        if i >= num_people:
             return
-        if len(t) == pplperteam:
+        if len(t) == ppl_per_team:
             acc.append((t, (set(players) - set(t))))
         else:
             permutation(i+1, t.copy())
@@ -89,20 +117,20 @@ def possible_teams(players):
 def team_split(state):
     '''
     Input:
-        state: a StatState class that holds all sorts of information on the video
+        state: a StatState class that holds all sorts of information
+                on the video
     Output:
         state: updated version of the input
     '''
     # list of player ids
     players = state.players
     # number of people total
-    numpeople = len(players)
     pos_lst = possession_list(state, players)
     state.possession_list = pos_lst
     connects = connections(pos_lst, players)
-    teams = possible_teams(players, numpeople)
-    bestteam = None
-    minCount = 0
+    teams = possible_teams(players)
+    best_team = None
+    min_count = 0
     for team in teams:
         count = 0
         team1 = list(team[0])
@@ -111,9 +139,9 @@ def team_split(state):
             for player2 in team2:
                 count += connects[player1 + player2]
                 count += connects[player2 + player1]
-        if count < minCount:
-            minCount = count
-            bestteam = team
-    state.team1 = bestteam[0]
-    state.team2 = bestteam[1]
+        if count < min_count:
+            min_count = count
+            best_team = team
+    state.team1 = best_team[0]
+    state.team2 = best_team[1]
     return state
