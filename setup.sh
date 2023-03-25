5bbc0edfad0e2756d4706a063eed630d23cd394f
Andrew Chang <yc16036@gmail.com>
2023-03-25 14:32:28 -0400

    Lint threshold (#9)
    * threshold
    
    * lint combine
    
    * lint fixed with threshold?
    
    * lint
    
    * above 7 now
    
    * minor error changes

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python3 -m venv venv
-source venv/Scripts/activate
-pip install --upgrade pip
-pip install -r requirements.txt
--- src/statrunner.py
+++ src/statrunner.py
@@ -8,10 +8,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from stats import statistics
 
 def load_config(path):
-	with open(path, 'r') as f:
-		config = yaml.safe_load(f)
+	with open(path, 'r') as file:
+		config = yaml.safe_load(file)
 	return config
-
 
 def run_statistics(video_path):
     statistics.load_video(video_path)
