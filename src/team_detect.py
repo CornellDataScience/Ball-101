b983b6bbb6c7269d6204e8986c489019b95b73a3
Pun Chaixanien <sc2343@cornell.edu>
2023-03-28 14:08:17 -0400

    fixed connections and added it to state

--- src/team_detect.py
+++ src/team_detect.py
@@ -4,6 +4,7 @@ This module contains functions that will find the best team split. It will
 also create a list of players in the order of ball possession.
 """
 
+
 def curr_possession(players, ball):
     '''
     Input:
@@ -31,6 +32,7 @@ def curr_possession(players, ball):
             max_area = area
             max_player = player
     return max_player
+
 
 def possession_list(state, thresh=20):
     '''
@@ -69,28 +71,46 @@ def possession_list(state, thresh=20):
             pos_lst.append(poss)
     return pos_lst
 
-def connections(pos_lst, players):
+# def connections(pos_lst, players):
+#     """
+#     Input:
+#         pos_lst [list]: list of player ids in the order of ball possession
+#                         throughout the video
+#         players [list]: list of player ids
+#     Output:
+#         connects [dict]: dictionary of connections between players
+#     """
+#     connects = {}
+#     for i, player in enumerate(players):
+#         for j, player2 in enumerate(players):
+#             if i == j:
+#                 continue
+#             name = player + player2
+#             connects.update({name: 0})
+
+#     curr = pos_lst[0]
+#     for i in range(1, len(pos_lst)):
+#         name = curr + pos_lst[i]
+#         connects[name] += 1
+#     return connects
+
+
+def connections(pos_lst, players, player_idx):
     """
     Input:
         pos_lst [list]: list of player ids in the order of ball possession
                         throughout the video
         players [list]: list of player ids
     Output:
-        connects [dict]: dictionary of connections between players
+        connects [list of lists]: 2D array of connections between players where
+                                    connects[i][j] is the number of times
+                                    player i passes to player j
     """
-    connects = {}
-    for i, player in enumerate(players):
-        for j, player2 in enumerate(players):
-            if i == j:
-                continue
-            name = player + player2
-            connects.update({name: 0})
-
-    curr = pos_lst[0]
-    for i in range(1, len(pos_lst)):
-        name = curr + pos_lst[i]
-        connects[name] += 1
+    connects = [[0 for _ in range(len(players))] for _ in range(len(players))]
+    for i in range(0, len(pos_lst)-1):
+        connects[player_idx.get(pos_lst[i])][player_idx.get(pos_lst[i+1])] += 1
     return connects
+
 
 def possible_teams(players):
     """
@@ -102,6 +122,7 @@ def possible_teams(players):
     num_people = len(players)
     ppl_per_team = num_people/2
     acc = []
+
     def permutation(i, t):
         if i >= num_people:
             return
@@ -114,6 +135,7 @@ def possible_teams(players):
     permutation(0, set())
     return acc
 
+
 def team_split(state):
     '''
     Input:
@@ -122,12 +144,13 @@ def team_split(state):
     Output:
         state: updated version of the input
     '''
-    # list of player ids
-    players = state.players
+    # dict of player stats
+    players = state.players.keys()
     # number of people total
     pos_lst = possession_list(state, players)
-    state.possession_list = pos_lst
-    connects = connections(pos_lst, players)
+    player_idx = {player: i for i, player in enumerate(players)}
+    connects = connections(pos_lst, players, player_idx)
+    state.passes = connects
     teams = possible_teams(players)
     best_team = None
     min_count = 0
@@ -137,8 +160,10 @@ def team_split(state):
         team2 = list(team[1])
         for player1 in team1:
             for player2 in team2:
-                count += connects[player1 + player2]
-                count += connects[player2 + player1]
+                count += connects[player_idx.get(player1)]
+                [player_idx.get(player2)]
+                count += connects[player_idx.get(player2)]
+                [player_idx.get(player1)]
         if count < min_count:
             min_count = count
             best_team = team
